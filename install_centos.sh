00b7ad07d62e165e06850f756e126c7b55780fa0
Sergei Riabov <alfatetan@gmail.com>
2024-03-25 01:08:36 -0700

    Added installation file for CentOS/RedHat Linux family

---
+++ install_centos.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "eBay automation QA testing installation..."
+
+echo "Packages updating and upgrating..."
+sudo yum update
+sudo yum upgrade
+
+echo "Checking Python 3 has been installed already or not"
+REQUIRED_PKG="python3"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+
+# If package is not exist - install python 3.x
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo yum --yes install $REQUIRED_PKG
+fi
+
+echo "Python environment installation..."
+python -m venv ./venv
+source ./venv/bin/activate
+
+# Install requirenments
+pip install -r requirenments.txt
+
+# Installation is done
+echo "Instalation is done! Enjoy new tests!"
